6dda61b7f211c81da47005236539554aa34191c2
norgeux <norgeux@insa-toulouse.fr>
2024-11-13 11:11:31 +0100

    Init BenevoleMS

---
+++ BenevoleMS/src/main/java/fr/insa/soa/BenevoleMS/controller/GestionBenevole.java
@@ -0,0 +1,31 @@
+package fr.insa.soa.BenevoleMS.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.insa.soa.BenevoleMS.model.Benevole;
+
+@RestController
+public class GestionBenevole {
+	
+	private int counterId = 1; //TODO : gerer counter
+	
+	
+	
+	@GetMapping("/benevoles")
+	public int benevoleNumber() {
+		return 100;
+	}
+	
+	@GetMapping(value="/benevoles/{id}")
+	public Benevole infosBenevole(@PathVariable int id) {
+		Benevole benevole = new Benevole("Esposito", "Cléa", 5, "rue de la paix", "espo@mimi.fr", "1234", id);
+		return benevole;
+	}
+	
+	//TODO : penser à incrémenter counter avant nouveau benevole
+	
+	
+}
+
